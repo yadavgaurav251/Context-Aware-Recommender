15d58028e9cad7e5eb1d11ebbe8d1a77d7ec1424
Anish Raj <ranishraj10282@gmail.com>
2020-12-07 17:42:32 +0530

    Fixed Some Issues

--- UI/Recommendation/urls.py
+++ UI/Recommendation/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('rec.urls')),
+    path('', include('rec.urls')),
 ]
--- UI/rec/views.py
+++ UI/rec/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     if request.method=='POST':
+        movies_id = [[862, 96], [8884, 95], [284, 93], [1907, 98], [1285, 95], [867, 87], [337, 95], [10527, 67], [1998, 78], [580, 95],  [10527, 95], [874, 95]]
         choice = request.POST['choice']
         movie = request.POST['movie']
-        number = request.POST['num_of_movie']
-        device = request.POST['device-size']
-        mood = request.POST['mood']
-        movies_id = [[862, 96], [8884, 95], [284, 93], [1907, 98], [1285, 95], [867, 87], [337, 95], [10527, 67], [1998, 78], [580, 95],  [10527, 95], [874, 95]]
+        userId = request.POST['user_Id']
+        if choice=="Yes":
+            numberOfPeople = request.POST['number_of_people'] 
+            device = request.POST['device-type']
+            mood = request.POST['mood']
+            family = request.POST.getlist('family[]')
         return render(request,'rec/index.html', {'display_rec': 'inline-block','movies_id':movies_id})
 
     return render(request,'rec/index.html',{'yes':False, 'no':False, 'movie':"Hello", 'number':0, 'display_rec': 'none'})
