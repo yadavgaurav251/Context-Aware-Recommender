7332a17cb753c452501c7c991edea6f1a511daf9
Gaurav Yadav <yadavgaurav251@gmail.com>
2020-12-10 01:04:30 +0530

    fixing bugs

--- UI/rec/views.py
+++ UI/rec/views.py
@@ -14,7 +14,6 @@ from datetime import datetime
 # In[3]:
 
 
-# The main Movies Metadata file
 meta = pd.read_csv('Dataset/movies_metadata.csv')
 
 # In[4]:
@@ -26,7 +25,6 @@ ratings = pd.read_csv('Dataset/ratings_small.csv')
 # In[5]:
 
 
-# Links of IMDb and TMDb
 links = pd.read_csv('Dataset/links_small.csv')
 
 # In[6]:
@@ -37,113 +35,87 @@ keywords = pd.read_csv('Dataset/keywords.csv')
 # In[7]:
 
 
-# -- Content-focused recommender --
+# -- Content filtering based Recommender
 
 meta['overview'] = meta['overview'].fillna('')
 
 # In[8]:
 
 
-# Check the datatype of "id" in movies_metadata.csv
 pd.DataFrame({'feature': meta.dtypes.index, 'dtype': meta.dtypes.values})
 
 # In[9]:
 
 
-meta = meta.drop([19730, 29503, 35587])  # Remove these ids to solve ValueError: "Unable to parse string..."
+meta = meta.drop([19730, 29503, 35587])
 
-# Convert object to int64 for compatibility during merging
 meta['id'] = pd.to_numeric(meta['id'])
-
-# Run  the following code for converting more than one value to integer
-# def convert_int(x):
-#     try:
-#         return int(x)
-#     except:
-#         return np.nan
 
 
 # In[10]:
 
 
-# Check the datatype of "tmdbId" in links_small.csv
 pd.DataFrame({'feature': links.dtypes.index, 'dtype': links.dtypes.values})
 
 # In[11]:
 
 
-# Convert float64 to int64
 col = np.array(links['tmdbId'], np.int64)
 links['tmdbId'] = col
 
 # In[12]:
 
 
-# Merge the dataframes on column "tmdbId"
 meta.rename(columns={'id': 'tmdbId'}, inplace=True)
 meta = pd.merge(meta, links, on='tmdbId')
 meta.drop(['imdb_id'], axis=1, inplace=True)
 meta.head()
 
-# Alternatively, run the following code to reduce the size of movies_metadata.csv to match links_small.csv
-# meta = meta[meta['tmdbId'].isin(links)]
-# meta.shape
-
 
 # In[13]:
 
 
-# Remove stop words and use TF-IDF vectorizer
 tfidf = TfidfVectorizer(stop_words='english')
-# Construct TF-IDF matrix
 tfidf_matrix = tfidf.fit_transform(meta['overview'])
-tfidf_matrix.shape
 
 # In[14]:
 
 
 # Compute cosine similarity
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-# Get corresponding indices of the movies
 indices = pd.Series(meta.index, index=meta['original_title']).drop_duplicates()
 
 
 # In[15]:
 
 
-# Recommendation function
 def recommend(title, cosine_sim=cosine_sim):
-    # Get the index of the movie that matches the title
     idx = indices[title]
-    # Get the pairwise similarity scores of all movies with the given movie
+    # pairwise similarity scores of movies with given movie
     sim_scores = list(enumerate(cosine_sim[idx]))
-    # Sort the movies based on the similarity scores
+    # Sort the movies on similarity scores
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    # Get the scores of the 15 most similar movies
+    # 15 most similar movies
     sim_scores = sim_scores[1:16]
-    # Get the movie indices
     movie_indices = [i[0] for i in sim_scores]
-    # Remove low-rated movies or outliers
+    # Remove low-rated
     for i in movie_indices:
         pop = meta.at[i, 'vote_average']
         if pop < 5 or pop > 10:
             movie_indices.remove(i)
 
-    # Return the most similar movies qualifying the 5.0 rating threshold
     return meta[['original_title', 'vote_average']].iloc[movie_indices]
 
 
 # In[16]:
 
 
-recommend('Iron Man')
 
 # In[17]:
 
+# Collaborative Filtering based Recommender
 
-# -- User-focused recommender --
-
-reader = Reader()  # Used to parse a file containing ratings
+reader = Reader()
 df = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
 kf = KFold(n_splits=5)
 kf.split(df)  # Split the data into folds
@@ -156,18 +128,14 @@ svd = pickle.load(open("movie_ml_model.sav", "rb"))
 # In[19]:
 
 
-# Check a random user's ratings
-ratings[ratings['userId'] == 10]
-
 # In[20]:
 
 
-# Read the smaller links file again
+# reload files
 links_df = pd.read_csv('Dataset/links_small.csv')
 col = np.array(links_df['tmdbId'], np.int64)
 links_df['tmdbId'] = col
 
-# Merge movies_metadata.csv and links_small.csv files
 links_df = links_df.merge(meta[['title', 'tmdbId']], on='tmdbId').set_index('title')
 links_index = links_df.set_index('tmdbId')  # For label indexing
 
@@ -175,22 +143,21 @@ links_index = links_df.set_index('tmdbId')  # For label indexing
 # In[21]:
 
 
-# Recommendation function
 def hybrid(userId, title):
     idx = indices[title]
-    tmdbId = links_df.loc[title]['tmdbId']  # Get the corresponding tmdb id
+    tmdbId = links_df.loc[title]['tmdbId']
 
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:31]  # Scores of the 30 most similar movies
+    sim_scores = sim_scores[1:31]  # calculating score of 30 similar movies
     movie_indices = [i[0] for i in sim_scores]
 
     movies = meta.iloc[movie_indices][['title', 'vote_average', 'tmdbId']]
     movies['est'] = movies['tmdbId'].apply(
         lambda x: svd.predict(userId, links_index.loc[x]['movieId']).est)  # Estimated prediction using svd
-    movies = movies.sort_values('est', ascending=False)  # Rank movies according to the predicted values
+    movies = movies.sort_values('est', ascending=False)  # Ranking movies according to predicted values
     movies.columns = ['Title', 'Vote Average', 'TMDb Id', 'Estimated Prediction']
-    return movies.head(30)  # Display top 15 similar movies
+    return movies.head(30)  # 30 similar movies
 
 
 # necessary functions for contextual_update function
@@ -254,7 +221,7 @@ def special_date(recommended_list, date_passed):
         new_movie = pd.DataFrame({"Title": ["Border", "Uri:The Surgical Strike"],
                                   "Vote Average": [6.8, 7.1],
                                   "TMDb Id": [33125, 554600],
-                                  "Estimated Prediction": [5.0, 5.0]
+                                  "Estimated Prediction": [4.8, 4.7]
                                   })
         new_list = pd.concat([new_movie, recommended_list])
     # Repubic Day
@@ -517,6 +484,7 @@ def contextual_update(list_passed, family=False, device="Mobile", no_of_people=1
 
     # Sorting the list for final result and comparing
     # print(list_passed)
+    recommended_list['Estimated Prediction'].clip(lower=0,upper =5,inplace=True)
     recommended_list.sort_values(by='Estimated Prediction', ascending=False, inplace=True)
     print(recommended_list)
     return recommended_list
